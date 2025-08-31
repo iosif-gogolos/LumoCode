4f12f935ba6d259d12c6db62c0cbd23cee022125
iosif-gogolos <iosif.gogolos@icloud.com>
2025-08-31 13:53:55 +0200

    Initial Commit

---
+++ LumoCode/LumoCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoCodeApp.swift
+//  LumoCode
+//
+//  Created by Iosif Gogolos on 31.08.25.
+//
+
+import SwiftUI
+
+@main
+struct LumoCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
